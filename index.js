e081e0db9abe5d93f44e115127a5d0860126e613
Jason Lee <jason@zerodevx.com>
2021-04-01 01:59:15 +0800

    Dead simple definitions

---
+++ index.js
@@ -0,0 +1,5 @@
+import { gzip, ungzip } from 'pako'
+import { fromUint8Array, toUint8Array } from 'js-base64'
+
+export const zipurl = data => fromUint8Array(gzip(data), true)
+export const unzipurl = data => ungzip(toUint8Array(data), { to: 'string' })
