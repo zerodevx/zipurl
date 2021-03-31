1ece79e2a9d00f27ebd246264de5d076fd9bd77b
Jason Lee <jason@zerodevx.com>
2021-04-01 01:54:53 +0800

    Setup browser tests

---
+++ index.spec.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+/* eslint-disable quotes */
+/* global chai, zipurl, unzipurl */
+
+mocha.setup({
+  ui: 'bdd'
+})
+
+describe('unit tests', () => {
+  const assert = chai.assert
+
+  describe('basic', () => {
+    it('encodes string', () => {
+      assert(zipurl('hello world!') === 'H4sIAAAAAAAAA8tIzcnJVyjPL8pJUQQAbcK0AwwAAAA')
+    })
+
+    it('decodes string', () => {
+      assert(unzipurl('H4sIAAAAAAAAA8tIzcnJVyjPL8pJUQQAbcK0AwwAAAA') === 'hello world!')
+    })
+  })
+})
+
+mocha.run()
