6a011567ddf2eb6c55fc7cdb40c0125b91e72bab
Jason Lee <jason@zerodevx.com>
2021-04-01 01:55:12 +0800

    Init rollup

---
+++ rollup.config.js
@@ -0,0 +1,29 @@
+import pkg from './package.json'
+import resolve from '@rollup/plugin-node-resolve'
+import { terser } from 'rollup-plugin-terser'
+import livereload from 'rollup-plugin-livereload'
+import serve from 'rollup-plugin-serve'
+
+const production = !process.env.ROLLUP_WATCH
+
+export default {
+  input: pkg.module,
+  output: {
+    format: 'umd',
+    name: 'window',
+    file: pkg.main,
+    extend: true
+  },
+  plugins: [
+    resolve(),
+    !production && serve({ port: 5000 }),
+    !production && livereload(),
+    production && terser({
+      module: true,
+      format: { comments: false }
+    })
+  ],
+  watch: {
+    clearScreen: false
+  }
+}
