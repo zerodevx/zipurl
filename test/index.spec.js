d8bc9d30f463ef647a1440efbdc2001956cb397b
Jason Lee <jason@zerodevx.com>
2022-02-08 01:27:29 +0800

    Move the tests

--- test/index.spec.js
+++ test/index.spec.js
@@ -11,11 +11,16 @@ describe('unit tests', () => {
 
   describe('basic', () => {
     it('encodes string', () => {
-      assert(zipurl('hello world!') === 'H4sIAAAAAAAAA8tIzcnJVyjPL8pJUQQAbcK0AwwAAAA')
+      assert(
+        zipurl('hello world!') === 'H4sIAAAAAAAAA8tIzcnJVyjPL8pJUQQAbcK0AwwAAAA'
+      )
     })
 
     it('decodes string', () => {
-      assert(unzipurl('H4sIAAAAAAAAA8tIzcnJVyjPL8pJUQQAbcK0AwwAAAA') === 'hello world!')
+      assert(
+        unzipurl('H4sIAAAAAAAAA8tIzcnJVyjPL8pJUQQAbcK0AwwAAAA') ===
+          'hello world!'
+      )
     })
   })
 })
