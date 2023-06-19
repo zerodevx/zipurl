9b0819c7f23b65df04854f4f7c58402af9c578b7
Jason Lee <jason@zerodevx.com>
2023-06-19 17:56:18 +0800

    Rename output file

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import terser from '@rollup/plugin-terser'
 export default {
   input: 'index.js',
   output: {
-    file: 'zipurl.min.js',
+    file: 'dist/index.umd.js',
     format: 'umd',
     name: 'window',
     extend: true
