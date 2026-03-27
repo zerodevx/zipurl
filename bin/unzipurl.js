4ccb4c04fd1597ffeeffe76231591cee73747159
Jason Lee <jason@zerodevx.com>
2026-03-28 02:14:19 +0800

    Upgrade CLI

--- bin/unzipurl.js
+++ bin/unzipurl.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
+import { text } from 'node:stream/consumers'
 import { unzipurl } from '../index.js'
-import cli from './cli.js'
 
-console.log(unzipurl(await cli()))
+const { argv, stdin, exit } = process
+const input = argv.slice(2).join(' ') || (stdin.isTTY ? '' : (await text(process.stdin)).trim())
+if (!input) {
+  console.error('Error: No arguments found!')
+  exit(1)
+}
+console.log(await unzipurl(input))
--- bin/zipurl.js
+++ bin/zipurl.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
+import { text } from 'node:stream/consumers'
 import { zipurl } from '../index.js'
-import cli from './cli.js'
 
-console.log(zipurl(await cli()))
+const { argv, stdin, exit } = process
+const input = argv.slice(2).join(' ') || (stdin.isTTY ? '' : (await text(process.stdin)).trim())
+if (!input) {
+  console.error('Error: No arguments found!')
+  exit(1)
+}
+console.log(await zipurl(input))
