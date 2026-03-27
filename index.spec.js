3329f3376b2f9213fb4a615e17eb0645e6c69a7c
Jason Lee <jason@zerodevx.com>
2026-03-28 02:10:50 +0800

    Remove `vite` and `playwright`

--- index.spec.js
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '@playwright/test'
-
-test('basic', async ({ page }) => {
-  await page.goto('/')
-  await expect(page.locator('.red')).toHaveCount(0)
-})
--- playwright.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('@playwright/test').PlaywrightTestConfig} */
-export default {
-  testMatch: 'index.spec.js',
-  webServer: {
-    command: 'npm run dev',
-    port: 5173,
-    reuseExistingServer: true
-  }
-}
